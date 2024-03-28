ed75480952eb068fbb2b17e313e2e52bc28cb417
Nidhi sahu27 <149890086+NidhiSahu2@users.noreply.github.com>
2024-03-28 12:37:58 +0530

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,19 @@
+//using a scope and block//
+{
+  let a=5;
+    console.log(a);
+
+
+}
+
+// again using a diff block//
+{
+    let b=45;
+    console .log(b);
+}
+//using a diff block //
+{
+    let c=34;
+    console.log(c);
+}
+//using 
